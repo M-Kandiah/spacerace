5fc888ea1235348f897cb623ab3dd5ff022051ba
ria10 <ria1099@gmail.com>
2021-10-25 12:48:44 +0100

    started lobby

--- space-race/src/App.js
+++ space-race/src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import {UserContext} from './contexts'
+import {useContext} from 'react';
 
 function App() {
   return (
     <>
+    <UserContext.Provider>
     <h1>Sam is cool</h1>
+    </UserContext.Provider>
     </>
   );
 }
