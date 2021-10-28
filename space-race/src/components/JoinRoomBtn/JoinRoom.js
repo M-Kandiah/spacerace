4475cadad7858714b26f8d944b446e2d98775862
MathusanKandiah <mathusankandiah@hotmail.com>
2021-10-28 18:29:35 +0100

    tooltips

--- space-race/src/components/JoinRoomBtn/JoinRoom.js
+++ space-race/src/components/JoinRoomBtn/JoinRoom.js
@@ -1,30 +1,40 @@
-import React, {useContext} from 'react'
-import {useHistory} from 'react-router-dom'
-import { Modal, Button } from 'react-bootstrap'
-import {UserContext} from '../../contexts'
+import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Modal, Button, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { UserContext } from '../../contexts'
 import { socket } from '../../App'
-import {io} from 'socket.io-client'
+import { io } from 'socket.io-client'
 import '../../App.css';
 
 export default function JoinRoom(props) {
     const history = useHistory()
     const [modalShow, setModalShow] = React.useState(false);
-    const {lobby, setLobby} = useContext(UserContext)
+    const { lobby, setLobby } = useContext(UserContext)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const user = localStorage.getItem('username')
-        const roomId =  e.target[0].value
-        socket.emit("join-room",roomId, user)
+        const roomId = e.target[0].value
+        socket.emit("join-room", roomId, user)
         setLobby('waiting');
         history.push("/waitingroom")
     }
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            Join a room with your friends!
+        </Tooltip>
+    );
 
     return (
         <div>
-            <button className="mainMenuBtns" aria-label="join" variant="primary" onClick={() => setModalShow(true)}>
-                JOIN ROOM
-            </button>
+            <OverlayTrigger placement="right"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltip}
+            >
+                <button className="mainMenuBtns" aria-label="join" variant="primary" onClick={() => setModalShow(true)}>
+                    JOIN ROOM
+                </button>
+            </OverlayTrigger>
             <Modal
                 {...props}
                 size="lg"
@@ -40,8 +50,10 @@ export default function JoinRoom(props) {
                 </Modal.Header>
                 <Modal.Body>
                     <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Enter Room Code"/>
-                    <input type="submit"/>
+                        <input type="text" placeholder="Enter Room Code" />
+
+                        <input type="submit" />
+
                     </form>
                 </Modal.Body>
 
--- space-race/src/components/leaderboardbtn/Leaderboard.js
+++ space-race/src/components/leaderboardbtn/Leaderboard.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import '../../App.css';
 
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
 export default function Leaderboard() {
     const handleClick = (e) => {
         e.preventDefault();
         window.location.href= '/leaderboard'
     }
+
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            Check where you stack up against your friends!
+        </Tooltip>
+    );
     return (
         <div >
+            <OverlayTrigger placement="right"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltip}
+            >
             <button className="mainMenuBtns" onClick={handleClick}>LEADERBOARDS</button>
+
+            </OverlayTrigger>
         </div>
     )
 }
